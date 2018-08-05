d5c43ef3527a0f055998d770afd43b61e4ce91cb
Amy Hart <xamyhart@gmail.com>
2018-08-05 21:19:21 +0100

    Completed Step 6 - Setup project, created first component, and created first unit test

---
+++ src/components/__tests__/components/location-details.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import LocationDetails from '../../location-details';
+
+it('renders the passed city and country in a h1 tag', () => {
+
+  const wrapper = Enzyme.shallow((
+    <LocationDetails city="Manchester" country="UK" />
+  ));
+
+  const text = wrapper.find('h1').text();
+
+  expect(text).toEqual('Manchester, UK');
+
+});
